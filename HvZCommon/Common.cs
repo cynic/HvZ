bf2af7ae60d2e356ad883411a3c74bad81cc7bc9
Christopher Albers <g13a2011@ru.ac.za>
2013-08-21 11:52:13 +0200

    * Moved common code from Human/Zombie to Walker + Added Types for walkers + Added a sock depo.

--- HvZCommon/Common.cs
+++ HvZCommon/Common.cs
@@ -6,25 +6,40 @@ using System.Threading.Tasks;
 
 namespace HvZCommon
 {
-   public interface ITakesSpace
-   {
-      Position Position { get; set; } // center of the object
-      double Radius { get; set; }
-   }
-
-    // heading is in degrees, 0 is directly upwards
-    public class Human : ITakesSpace
+    public interface ITakesSpace
     {
-       public Position Position { get; set; }
-       public double Heading { get; set; }
-       public double Radius { get; set; }
+        Position Position { get; set; } // center of the object
+        double Radius { get; set; }
     }
 
-    public class Zombie : ITakesSpace
+    public class Walker : ITakesSpace
     {
-       public Position Position { get; set; }
-       public double Heading { get; set; }
-       public double Radius { get; set; }
+        public Position Position { get; set; }
+        public double Radius { get; set; }
+
+        public WalkerType type { get; set; }
+
+        public double Heading { get; set; }
+        public double Speed { get; set; }
+
+        public bool isExploding() {
+            return type.HasFlag(WalkerType.EXPLODING);
+        }
+
+        public bool isPlayerControlled() {
+            return type.HasFlag(WalkerType.SMART);
+        }
+    }
+
+    // heading is in degrees, 0 is directly upwards
+    public class Human : Walker
+    {
+        //does human things
+    }
+
+    public class Zombie : Walker
+    {
+        //does zombie things
     }
 
     public class Position
@@ -39,10 +54,29 @@ namespace HvZCommon
        public UInt32 GameTime { get; set; }
     }
 
+    /// <summary>
+    /// Supply point provides food + health care.
+    /// </summary>
     public class ResupplyPoint : ITakesSpace
     {
-       public Position Position { get; set; }
-       public double Radius { get; set; }
+        public Position Position { get; set; }
+        public double Radius { get; set; }
+
+        public void Supply()
+        {
+            //give stuff
+        }
+    }
+
+    /// <summary>
+    /// Sock Depo provides sock refills/ammunition
+    /// </summary>
+    public class SockDepo : ResupplyPoint
+    {
+        public override void Supply()
+        {
+            //give socks
+        }
     }
 
     public class Map
