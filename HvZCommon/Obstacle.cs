e298053d499c4c3143dd76b27f67ba376a10fab6
Yusuf M Motara <y.motara@ru.ac.za>
2013-10-12 06:28:11 +0200

    String representations for spawnpoints & obstacles

--- HvZCommon/Obstacle.cs
+++ HvZCommon/Obstacle.cs
@@ -7,20 +7,26 @@ namespace HvZ.Common {
     internal class Obstacle : IVisual {
         public Position Position { get; private set; } // center of the object
         public double Radius { get; private set; }
-        public string Texture { get { return "obstacle"; } }
+        string IVisual.Texture { get { return "obstacle"; } }
         public Obstacle(double x, double y, double radius) {
             Position = new Position(x, y);
             Radius = radius;
+        }
+        public override string ToString() {
+            return String.Format("Obstacle at {0}, radius {1}", Position, Radius);
         }
     }
 
     internal class SpawnPoint : IVisual {
         public Position Position { get; private set; } // center of the object
         public double Radius { get; private set; }
-        public string Texture { get { return "spawnpoint"; } }
+        string IVisual.Texture { get { return "spawnpoint"; } }
         public SpawnPoint(double x, double y, double radius) {
             Position = new Position(x, y);
             Radius = radius;
+        }
+        public override string ToString() {
+            return String.Format("SpawnPoint/Obstacle at {0}, radius {1}", Position, Radius);
         }
     }
 }
