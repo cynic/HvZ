4efc505aacc19780372aed5adb1344a79375d87a
Yusuf M Motara <y.motara@ru.ac.za>
2013-10-12 06:20:47 +0200

    Another overload for Intersects; returning at least 0.1 magnitude from angle functions; and a CollisionDistance function (untested at present)

--- HvZCommon/Utilities.cs
+++ HvZCommon/Utilities.cs
@@ -10,34 +10,78 @@ using HvZ.Common;
 namespace HvZ {
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class Extensions {
-        internal static readonly Random rand = new Random();
-
+        /// <summary>
+        /// Returns true if two items on the map overlap
+        /// </summary>
         public static bool Intersects(this ITakeSpace a, ITakeSpace b) {
-            return Intersects(a, b.Position.X, b.Position.Y, b.Radius);
+            return Intersects(a.Position.X, a.Position.Y, a.Radius, b.Position.X, b.Position.Y, b.Radius);
         }
 
-        public static bool Intersects(this ITakeSpace a, double x, double y, double radius) {
+        /// <summary>
+        /// Returns the distance after which a walker's path, at the current heading, would probably collide with the specified item.
+        /// </summary>
+        /// <returns>A positive distance at which a collision would occur, OR a negative number when no collision would occur.</returns>
+        internal static double CollisionDistance(this IWalker a, ITakeSpace b, double proposedDistanceForward) {
+            if (proposedDistanceForward < 0)
+                throw new ArgumentException("proposedDistanceForward can't be negative.", "proposedDistanceForward");
+            // if the player goes forward here, would they intersect?
+            double distTraveled = 0.0;
+            var h = a.Heading.ToRadians();
+            double distPerStep = Math.Max(0.05, (Math.Min(WorldConstants.HumanSpeed, WorldConstants.ZombieSpeed) / WorldConstants.StepsPerTurn));
+            var distXPerStep = distPerStep * Math.Sin(h);
+            var distYPerStep = -distPerStep * Math.Cos(h);
+            var curX = a.Position.X;
+            var curY = a.Position.Y;
+            while (distTraveled < proposedDistanceForward) {
+                if (Intersects(curX, curY, a.Radius, b.Position.X, b.Position.Y, b.Radius))
+                    return distTraveled;
+                distTraveled += distPerStep;
+                curX += distXPerStep;
+                curY += distYPerStep;
+            }
+            return -1;
+        }
+
+        internal static bool Intersects(this ITakeSpace a, double bx, double by, double bradius) {
+            return Intersects(a.Position.X, a.Position.Y, a.Radius, bx, by, bradius);
+        }
+
+        internal static bool Intersects(double ax, double ay, double aradius, double bx, double by, double bradius) {
             // thanks to: http://stackoverflow.com/questions/8367512/algorithm-to-detect-if-a-circles-intersect-with-any-other-circle-in-the-same-pla
-            var dX = a.Position.X - x;
-            var dY = a.Position.Y - y;
+            var dX = ax - bx;
+            var dY = ay - by;
             var distBetween = Math.Sqrt((dX * dX) + (dY * dY));
-            var sum = Math.Abs(a.Radius + radius);
+            var sum = Math.Abs(aradius + bradius);
             //var diff = Math.Abs(a.Radius - radius);
             return /*distBetween >= diff &&*/ distBetween <= sum;
         }
 
-        /// <summary>Calculates straight line distance between two Entities (apparently tested)</summary>
+        /// <summary>Calculates straight line distance between two things on the map</summary>
         public static double DistanceFrom(this ITakeSpace a, ITakeSpace b) {
             double distX = a.Position.X - b.Position.X;
             double distY = a.Position.Y - b.Position.Y;
             return Math.Sqrt(distX * distX + distY * distY);
         }
 
-        public static bool IsCloseEnoughToUse(this IWalker a, ITakeSpace b) {
+        /// <summary>
+        /// Returns true if the player is close enough to interact with (e.g., bite or take socks from) the specified thing on the map
+        /// </summary>
+        public static bool IsCloseEnoughToInteractWith(this IWalker a, ITakeSpace b) {
             return a.DistanceFrom(b) - a.Radius - b.Radius <= WorldConstants.InteractionDistance;
         }
 
-        /// <summary>Calculates the number of degrees of turn needed to face a particular thing</summary>
+        /// <summary>
+        /// Gives the smallest angle that the walker must turn in order to have a particular heading.
+        /// </summary>
+        public static double AngleToHeading(this IWalker a, double heading) {
+            var desired = (90.0 - heading.PositiveAngle()).ToRadians(); // relative to the x-axis
+            double curHeading = (90.0 - a.Heading).ToRadians(); // relative to the x-axis
+            Vector vHeading = new Vector(-Math.Cos(curHeading), Math.Sin(curHeading));
+            Vector vTarget = new Vector(-Math.Cos(desired), Math.Sin(desired));
+            return Vector.AngleBetween(vHeading, vTarget).MinimumAngle();
+        }
+
+        /// <summary>Calculates how many degrees a zombie/human should turn to face a particular thing</summary>
         /// <returns>The angle to turn.  A negative number means a left turn, a positive number means a right turn.</returns>
         public static double AngleTo(this IWalker a, ITakeSpace b) {
             /*
@@ -57,19 +101,29 @@ namespace HvZ {
             // let the built-in vector math sort it all out :-).
             Vector vHeading = new Vector(a.Position.X - xC, a.Position.Y - yC);
             Vector vTarget = new Vector(a.Position.X - b.Position.X, a.Position.Y - b.Position.Y);
-            return Vector.AngleBetween(vHeading, vTarget);
+            return Vector.AngleBetween(vHeading, vTarget).MinimumAngle();
         }
 
+        /// <summary>Calculates how many degrees a zombie/human should turn to face away from a particular thing</summary>
         public static double AngleAwayFrom(this IWalker a, ITakeSpace b) {
             var angleTo = a.AngleTo(b);
             if (angleTo + 180.0 >= 180.0)
-                return 180.0 - angleTo;
-            return angleTo + 180.0;
+                return (180.0 - angleTo).MinimumAngle();
+            return (angleTo + 180.0).MinimumAngle();
         }
 
+        /// <summary>Calculates how many degrees a zombie/human should turn to avoid a particular thing</summary>
         public static double AngleAvoiding(this IWalker a, ITakeSpace b) {
             var angleTo = a.AngleTo(b);
-            return angleTo + 90.0;
+            if (Math.Abs(angleTo + 90.0) > Math.Abs(angleTo - 90.0))
+                return (angleTo - 90.0).MinimumAngle();
+            return (angleTo + 90.0).MinimumAngle();
+        }
+
+        internal static double MinimumAngle(this double x) {
+            if (x == 0.0) return x;
+            if (Math.Abs(x) < 0.01) return 0.01 * Math.Sign(x);
+            return x;
         }
 
         internal static double PositiveAngle(this double x) {
@@ -82,18 +136,6 @@ namespace HvZ {
 
         public static double ToRadians(this double x) {
             return (x * Math.PI) / 180.0;
-        }
-
-        public static T PickOne<T>(this T[] array) {
-            return array[rand.Next(array.Length)];
-        }
-
-        public static T PickOne<T>(this IEnumerable<T> list) {
-            return list.ElementAt(rand.Next(list.Count()));
-        }
-
-        public static T[] Tail<T>(this T[] array) {
-            return array.Skip(1).ToArray();
         }
     }
 }
