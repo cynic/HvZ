99038b78275d4d5e901f2decb6198e34c2b256e1
Yusuf M Motara <y.motara@ru.ac.za>
2013-10-17 13:10:54 +0200

    Fill-in-the-methods structure for a HumanAI

---
+++ HumansVsZombies/AI/MyHumanAI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HvZ;
+
+namespace HumansVsZombies
+{
+    public class MyHumanAI : IHumanAI
+    {
+        public void Collision(IHumanPlayer player, Edge edge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Collision(IHumanPlayer player, ITakeSpace other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoSomething(IHumanPlayer player, List<IWalker> zombies, List<IWalker> humans, List<ITakeSpace> obstacles, List<ResupplyPoint> resupply)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Failure(string reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
